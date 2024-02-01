064ea288fdc1da1d5d355b0d5debbdcb04539b53
Anupam Yadav <anupamyadav667@gmail.com>
2024-02-01 19:02:12 +0530

    Project setup

---
+++ src/Config/config.js
@@ -0,0 +1,10 @@
+const conf ={
+    appwriteurl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteCollectionid:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteDatabaseid:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_URL),
+}
+
+
+export default conf
