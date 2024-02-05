cc60c893c7bb22a78e476d717e78e74dfa671b48
Anupam Yadav <anupamyadav667@gmail.com>
2024-02-06 02:47:55 +0530

    Added redux store to track the user if it logged in or logged out

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
+import { useState ,useEffect} from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
 import { Footer, Header } from './components'
-
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
 function App() {
 
+  const [loading,setLoading]=useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
+
   return (
-    <>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
       <Header/>
-      Hello!
       <Footer/>
-    </>
+      </div>
+    </div>
   )
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(){
     return(
-        <h1 className="text-3xl font-bold underline">
+        <h1>
             Footer
         </h1>
     )
